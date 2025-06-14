3d9f9c0545eb93e7d52abef67a2be4a258190a1e
ohmdevplayz <tuf47.os@gmail.com>
2025-06-14 17:06:44 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index')
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
